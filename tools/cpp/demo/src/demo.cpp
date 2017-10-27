8ea27eb3d88f3b88c1507b9de17157d57cc59ddb
Amir Bawab <amirbawab@gmail.com>
2017-10-27 19:19:52 -0400

    Enhanced demo code

--- tools/cpp/demo/src/demo.cpp
+++ tools/cpp/demo/src/demo.cpp
@@ -2,39 +2,123 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <fstream>
-#include <sstream>
-#include <string>
+#include <unistd.h>
+#include <getopt.h>
 
-using namespace cv;
-using namespace std;
+#define ROWS 64
+#define COLS 64
+#define PIXLES ROWS * COLS
+
+// Global arguments
+std::string g_inputFile;
+int g_number = 1;
+
+/**
+ * Print program usage to stdout
+ */
+void printUsage() {
+    std::cout
+            << "demo - Demo images" << std::endl
+            << "    -i, --input     Input file" << std::endl
+            << "    -n, --number    Number of images to show" << std::endl
+            << "    -h, --help      Display this help message" << std::endl;
+}
+
+/**
+ * Read a matrix from a file
+ * @param input
+ * @return matrix
+ */
+cv::Mat file2Mat(std::ifstream &input) {
+    cv::Mat out(ROWS, COLS, CV_8UC1);
+    int row = 0;
+    int col = 0;
+    for(int i=0; i < PIXLES; i++) {
+
+        // Load input from file
+        double val;
+        input >> val;
+
+        // Update pixel
+        out.at<uchar>(row, col) = (uchar)(255 - val);
+        if(++col == COLS) {
+            row++;
+            col = 0;
+        }
+    }
+    return out;
+}
+
+/**
+ * Show images
+ * @param fileName
+ * @param images
+ */
+void showImages(std::string fileName, int images) {
+    std::ifstream input(fileName);
+
+    // Set image configuration
+    if(input.is_open()) {
+        while(images-- > 0) {
+
+            // Load matrix from file
+            cv::Mat out = file2Mat(input);
+
+            // Set a unique window name
+            std::stringstream winName;
+            winName << "Image " << images;
+            cv::namedWindow(winName.str(), cv::WINDOW_AUTOSIZE);
+            cv::imshow(winName.str(), out);
+        }
+    } else {
+        std::cerr << "Error opening file" << std::endl;
+    }
+    cv::waitKey(0);
+}
+
+
+/**
+ * Initialize parameters
+ * @param argc
+ * @param argv
+ */
+void initParams(int argc, char *argv[]) {
+    struct option longOptions[] = {
+            {"input", required_argument, 0, 'i'},
+            {"number", required_argument, 0, 'n'},
+            {"help",   no_argument,       0, 'h'},
+            {0, 0,                        0, 0}
+    };
+
+    int optionIndex = 0;
+    int c;
+    while ((c = getopt_long(argc, argv, "hi:n:", longOptions, &optionIndex)) != -1) {
+        switch (c) {
+            case 'i':
+                g_inputFile = optarg;
+                break;
+            case 'n':
+                g_number = atoi(optarg);
+                break;
+            case 'h':
+            default:
+                break;
+        }
+    }
+}
 
 int main( int argc, char** argv ) {
-    std::ifstream inExample("/tmp/img.csv");
-    int rows = 64;
-    int cols = 64;
-    std::string lineMat;
-    while (std::getline(inExample, lineMat)) {
-        Mat out = Mat::zeros(rows, cols, CV_64F);
-        int row = 0;
-        int col = 0;
-        std::istringstream iss(lineMat);
-        std::string token;
-        while(std::getline(iss, token, ',')) {
-            out.at<double>(row, col) = atof(token.c_str())/100;
-            col++;
-            if(col == 64) {
-                row++;
-                col = 0;
-            }
-        }
+    // Initialize parameters
+    initParams(argc, argv);
 
-        std::cout << out << std::endl;
-        std::stringstream winName;
-        winName << "Image " << row << ":" << col;
-        namedWindow(winName.str(), WINDOW_AUTOSIZE );
-        imshow(winName.str(), out);
+    // Check for missing params
+    if(g_inputFile.empty()) {
+        printUsage();
+        return 0;
     }
-    waitKey(8000);
+
+    // Show images
+    showImages(g_inputFile, g_number);
     return 0;
 }
 
