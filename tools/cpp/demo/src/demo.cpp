939d55233ba1454417990c43ed13c29ad0665c60
Amir Bawab <amirbawab@gmail.com>
2017-10-27 13:37:45 -0400

    Working example

--- tools/cpp/demo/src/demo.cpp
+++ tools/cpp/demo/src/demo.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv ) {
+    std::ifstream inExample("/tmp/img.csv");
+    int rows = 64;
+    int cols = 64;
+    std::string lineMat;
+    while (std::getline(inExample, lineMat)) {
+        Mat out = Mat::zeros(rows, cols, CV_64F);
+        int row = 0;
+        int col = 0;
+        std::istringstream iss(lineMat);
+        std::string token;
+        while(std::getline(iss, token, ',')) {
+            out.at<double>(row, col) = atof(token.c_str())/100;
+            col++;
+            if(col == 64) {
+                row++;
+                col = 0;
+            }
+        }
 
-    // Create matrix from image
-    Mat imageSrc = imread("/tmp/image.jpg", CV_LOAD_IMAGE_COLOR);
-    Mat imageDest;
-
-    // Create windows
-    namedWindow("My window 1", WINDOW_AUTOSIZE);
-    namedWindow("My window 2", WINDOW_AUTOSIZE);
-    namedWindow("blue", WINDOW_AUTOSIZE);
-    namedWindow("green", WINDOW_AUTOSIZE);
-    namedWindow("red", WINDOW_AUTOSIZE);
-
-    // Prepare split vector
-    vector<Mat> bgr;
-
-    // Split channels
-    split(imageSrc, bgr);
-
-    // Merge channels again
-    merge(bgr, imageDest);
-
-    // Show images in windows
-    imshow("My window 1", imageSrc);
-    imshow("blue", bgr[0]);
-    imshow("green", bgr[1]);
-    imshow("red", bgr[2]);
-    imshow("My window 2", imageDest);
-
-    waitKey(0);
+        std::cout << out << std::endl;
+        std::stringstream winName;
+        winName << "Image " << row << ":" << col;
+        namedWindow(winName.str(), WINDOW_AUTOSIZE );
+        imshow(winName.str(), out);
+    }
+    waitKey(8000);
     return 0;
 }
 
