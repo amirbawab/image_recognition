e98cc7fa85014eef015612d69909c2a981db2d3b
Amir Bawab <amirbawab@gmail.com>
2017-11-01 22:10:10 -0400

    Added tesseract example

---
+++ tools/cpp/tesseract/src/tesseract.cpp
@@ -0,0 +1,30 @@
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+int main()
+{
+    char *outText;
+
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    Pix *image = pixRead("/tmp/images/777/0_7.jpg");
+    api->SetImage(image);
+    api->SetVariable("tessedit_char_whitelist","0123456789AM");
+
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+
+    // Destroy used object and release memory
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
+
+    return 0;
+}
